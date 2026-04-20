ca56b7a128eb0a2099b5c0ea8f2702d41d19e206
Marwan Elkarif <marwanelkarf@gmail.com>
2026-04-20 19:12:04 +0200

    add (Home - Login - Logout) views and templates - redirect after register to login

--- BookStore/BookApp/urls.py
+++ BookStore/BookApp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path 
-from .views import Register
+from .views import Register , Login , Logout , Home
 urlpatterns = [
     path("register/", Register, name="register"),
+    path("login/", Login , name ="login"),
+    path("logout/", Logout , name ="logout"),
+    path("", Home, name="home"),
 ]
--- BookStore/BookApp/views.py
+++ BookStore/BookApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render , redirect
 from .forms import BookForm
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login , logout , authenticate
 # Create your views here.
 
 def Register(request):   
@@ -8,10 +9,53 @@ def Register(request):
         
         form = UserCreationForm(request.POST)
         if form.is_valid():
+            username = form.cleaned_data["username"]
+            password1 = form.cleaned_data["password1"]
+            password2 = form.cleaned_data["password2"]
             form.save()
+            return redirect("login")
 
     else:
         form = UserCreationForm()
 
     context = {"form": form}
     return render(request, "BookApp/register.html", context)
+
+
+def Home(request):
+    return render(request, "BookApp/home.html")
+
+
+def Login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        # True , False
+        user = authenticate(request , username = username , password = password)
+        
+        if user :
+            login(request,user)
+            return redirect("home")
+        
+        else:
+            raise ValueError("Invalid data")
+    return render(request, "BookApp/login.html")
+
+
+def Logout(request):
+
+    logout(request)
+    return redirect("login")
+
+
+
+   
+
+
+
+
+
+
+
+
+
